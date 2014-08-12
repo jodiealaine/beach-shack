e9acf43c41fc129d5e44048d25f0037d73859d7b
Jodie Alaine Parker <me@jodiealaine.com>
2014-08-13 00:38:12 +0100

    Added the ability to pass real parameters

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,15 +4,15 @@ class ChargesController < ApplicationController
 	  @amount = 500
 
 	  customer = Stripe::Customer.create(
-	    :email => 'example@stripe.com',
+	    :email => params[:stripeEmail],
 	    :card  => params[:stripeToken]
 	  )
 
 	  charge = Stripe::Charge.create(
 	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
+	    :amount      => params[:amount],
+	    :description => 'Growth Hacking Course',
+	    :currency    => 'gbp'
 	  )
 
 		rescue Stripe::CardError => e
