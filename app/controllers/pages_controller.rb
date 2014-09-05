6492226370b18cc6c9ac2d30f25c795746837ebc
Jodie Alaine Parker <me@jodiealaine.com>
2014-09-05 10:31:22 +0100

    Added Subscription Product Seed Data and updated pages controller to use subscription product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
 	def home
-		@product = Product.find_by_sku("BLP1")
+		@product = Product.find_by_sku("BLP2")
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,26 @@ beach_life = Product.create(title: "Beach Life", subtitle: "Beach Life Package",
                 <span class="pull-right">Personal Use / 1 Commercial Use</span>
               </li>
             </ul>}, download_url: "https://s3.amazonaws.com/beach-shack/keep-calm-and-say-hi-343.png")
+beach_life_subscription = Product.create(title: "Beach Life", subtitle: "Beach Life Subscription", author: "Jodie Parker", price: "4.99", sku: "BLP2", 
+		description: %{<p>Beach Life is a photograph collection of life on the beach around West Cornwall.</p>
+
+            <p>In this subscription, You will have access to the whole of the Beach Life library that best portray life on the beach through the seasons.</p>
+            <p><strong>What You'll Get</strong></p>
+            <ul class="no-indent">
+                <li>Beach Life Subscription</li>
+                <li>6 months unlimited access</li>
+                <li>Standard Licence to use personally</li>
+                <li>Commercial Licence to use for 1 website</li>
+            </ul>}, 
+   	author_description: %{<p>Hey, I'm Jodie Parker, co-founder of <a href="http://www.westock.co" target="_blank">Beach Life</a> where I photograph the best the West Country has to offer.</p>
+                <p>Follow me on Twitter <a href="https://twitter.com/beachmuser" target="_blank">@beachmuser</a></p>
+            </div>}, author_image_name: "teacher-image.jpg", 
+    details: %{  
+            <ul id="details-infobox" class="list-group">
+              <li class="list-group-item active clearfix">DETAILS</li>
+              <li class="list-group-item">6 Months Unlimited Access</li>
+              <li class="list-group-item clearfix">
+                <span class="pull-left content-qualifier">Terms of Use</span>
+                <span class="pull-right">Personal Use / 1 Commercial Use</span>
+              </li>
+            </ul>}, download_url: "https://s3.amazonaws.com/beach-shack/keep-calm-and-say-hi-343.png")
