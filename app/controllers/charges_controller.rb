e09ff70ad90be8c185bac245504ceb99926282ef
Jodie Alaine Parker <me@jodiealaine.com>
2014-09-05 10:59:56 +0100

    Implemented subscription based product BLP2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,21 +1,22 @@
 class ChargesController < ApplicationController
 	def create
-		product = Product.find_by_sku("BLP1")
+		product = Product.find_by_sku("BLP2")
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
+	    :card  => params[:stripeToken],
+	    :plan  => "BLP2"
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => product.price_in_pence,
-	    :description => product.full_description,
-	    :currency    => 'gbp'
-	  )
+	  # charge = Stripe::Charge.create(
+	  #   :customer    => customer.id,
+	  #   :amount      => product.price_in_pence,
+	  #   :description => product.full_description,
+	  #   :currency    => 'gbp'
+	  # )
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-    amount: product.price_in_pence, description: charge.description, currency: charge.currency,
+    amount: product.price_in_pence, description: product.full_description, currency: 'gbp',
     customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 		redirect_to purchase
